84734b38de196be7b7ff0fb75791a7fc807640e8
lukskyfly <lukasz.grazda@gmail.com>
2023-02-26 23:02:59 +0100

    added color to hover icons and turn of nav after click

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 	const nav = document.querySelector('.navbar')
+	const navCollapse = document.querySelector('.navbar-collapse')
+	const navList = document.querySelectorAll('.nav-link')
 
 	function addShadow() {
 		if (window.scrollY >= 100) {
@@ -8,5 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
 			nav.classList.remove('shadow-bg')
 		}
 	}
+
+	navList.forEach(item => item.addEventListener('click', () => navCollapse.classList.remove('show')))
 	window.addEventListener('scroll', addShadow)
 })
