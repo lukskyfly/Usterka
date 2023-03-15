d9bc56562912c3d42b2b926c515935f92f87cf24
lukskyfly <lukasz.grazda@gmail.com>
2023-03-15 23:14:59 +0100

    added fix bug

--- js/settingsSlick.js
+++ js/settingsSlick.js
@@ -1,5 +1,7 @@
 // $('.team-carousel').slick({
-// 	infinite: true,
-// 	slidesToShow: 3,
-// 	slidesToScroll: 3,
-// })
+//     dots: true,
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 1,
+//     adaptiveHeight: true
+//   });
